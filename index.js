08fef04816e54cf33b5165466b6ae79657408b50
Nelson Pecora <nelson@yoshokatana.com>
2015-01-28 14:46:36 -0500

    title screen

--- index.js
+++ index.js
@@ -3,12 +3,40 @@
 
 var program = require('commander'),
   pkg = require('./package.json'),
+  chalk = require('chalk'),
   checkRadios = require('./lib/check-radios');
+
+function showTitle() {
+  console.log(chalk.green(
+    ' __  __   _    ____ _____ _____ ____ _____ \n' +
+    '|  \/  |  / \\  / ___|  ___| ____/ ___|_   _|\n' +
+    '| |\/| | / _ \\| |  _| |_  |  _| \\___ \\ | |  \n' +
+    '| | | |/ ___ \\ |_| |  _| | |___ ___) || |  \n' +
+    '|_| |_/_/   \\_\\____|_|   |_____|____/ |_|  \n' +
+    ' ____      _    ____ ___ ___                \n' +
+    '|  _ \\    / \\  |  _ \\_ _/ _ \\               \n' +
+    '| |_) |  / _ \\ | | | | | | | |              \n' +
+    '|  _ <  / ___ \\| |_| | | |_| |              \n' +
+    '|_| \\_\\/_/   \\_\\____/___\\___/               \n' +
+    ' ______   ______ _____ _____ __  __         \n' +
+    '/ ___\\ \\ / / ___|_   _| ____|  \\/  |        \n' +
+    '\\___  \\ V /\\___ \\ | | |  _| | |\\/| |        \n' +
+    ' ___) || |  ___) || | | |___| |  | |        \n' +
+    '|____/ |_| |____/ |_| |_____|_|  |_|        '
+    ));
+}
 
 program
   .version(pkg.version)
   .option('-a, --add <items>', 'Add radios, e.g. 1-10 or 12,13')
-  .option('-r, --remove <items>', 'Remove radios, e.g. 3,4 or 1-6')
-  .parse(process.argv);
+  .option('-r, --remove <items>', 'Remove radios, e.g. 3,4 or 1-6');
 
-checkRadios(program);
+program.parse(process.argv);
+
+if (program.add || program.remove) {
+  // see if they want to modify radios
+  checkRadios(program);
+} else {
+  showTitle();
+  // ok, start the actual program
+}
